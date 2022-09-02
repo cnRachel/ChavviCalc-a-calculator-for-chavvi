78d988b30d1cfcd4c6f08312448f28004f416456
Rachel Chen <rachelchen110@gmail.com>
2022-09-01 19:59:14 -0700

    Used ChavviCalcExample.java file

--- chavvicalc/src/main/java/com/example/App.java
+++ chavvicalc/src/main/java/com/example/App.java
@@ -1,13 +1,79 @@
 package com.example;
 
-/**
- * Hello world!
- *
+import java.util.*;
+
+/*
+ * chavvi calc calculator
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    Character command = '_';
+
+    // loop until user quits
+    while (command != 'q') {
+      printMenu();
+      System.out.print("Enter a command: ");
+      command = menuGetCommand(scan);
+
+      executeCommand(scan, command);
     }
+
+    scan.close();
+  }
+
+  //
+  // menu functions
+  //
+  private static void printMenuLine() {
+    System.out.println(
+      "----------------------------------------------------------"
+    );
+  }
+
+  private static void printMenuCommand(Character command, String desc) {
+    System.out.printf("%s\t%s\n", command, desc);
+  }
+
+  // prints the menu
+  public static void printMenu() {
+    printMenuLine();
+    System.out.println("ChavviCalc");
+    printMenuLine();
+
+    printMenuCommand('q', "Quit");
+
+    printMenuLine();
+  }
+
+  // get first character from input
+  private static Character menuGetCommand(Scanner scan) {
+    Character command = '_';
+
+    String rawInput = scan.nextLine();
+
+    if (rawInput.length() > 0) {
+      rawInput = rawInput.toLowerCase();
+      command = rawInput.charAt(0);
+    }
+
+    return command;
+  }
+
+  // calculator functions
+  private static Boolean executeCommand(Scanner scan, Character command) {
+    Boolean success = true;
+
+    switch (command) {
+      case 'q':
+        System.out.println("Thank you for using Chavvi Calc");
+        break;
+      default:
+        System.out.println("ERROR: Unknown commmand");
+        success = false;
+    }
+
+    return success;
+  }
 }
